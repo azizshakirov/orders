626b1e165b46675835de70087ac85f6980388d0c
azizgo <shakirovazizshavkatovich@gmail.com>
2025-12-12 09:25:51 +0500

    finish 2 task

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import json
 with open("orders.json", 'r', encoding="utf-8") as file:
     orders = json.load(file)
 
-print(orders)
+customers = []
+for order in orders:
+    name = order.get('customer_name')
+    items = order.get("items")
+    total = sum([item['price']*item['quantity'] for item in items])
+    customer = {'name' : name, "total" : total}
+    customers.append(customer)
+
+for customer in customers:
+    print(f"{customer['name']} -> {customer['total']} USD" )
